39c9b68cfa31b412fd8d6c526a9a6789326844b3
Fábio Pereira <fabio.pereira@labtrans.ufsc.br>
2022-05-11 23:58:21 -0300

    fix: change openApi base path

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
     .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 }
